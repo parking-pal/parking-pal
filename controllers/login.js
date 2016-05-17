08137969944bd6856298efb36d397dfa951bdd56
BenByer <benbyer@shaw.ca>
2016-05-17 10:46:05 -0700

    Added login stuff

--- controllers/login.js
+++ controllers/login.js
@@ -1,11 +1,13 @@
+var passport = require('passport');
+
 module.exports = function(app) {
-      app.get('/login', function (req, res) {
-      res.render('login', {redirect: req.query.redirect});
+  app.get('/login', function (req, res) {
+    res.render('login', {redirect: req.query.redirect});
   });
-      app.post('/login', function (req, res) {
 
-        // TODO: Login
-
-        res.redirect(req.body.redirect);
-      })
-}
+  app.post('/login', 
+    passport.authenticate('local', { failureRedirect: '/login' }), function(req, res, next) {
+      return res.redirect('/');  
+  });
+  
+};
--- seeders/20160516182600-users-seeds.js
+++ seeders/20160516182600-users-seeds.js
@@ -7,6 +7,7 @@ module.exports = {
         last_name: 'Ramone',
         email: 'joey@ramones.com',
         phone: '(250) 123-4568',
+        password: 'Joey123',
         createdAt: new Date(2016, 5, 10, 12, 24, 34),
         updatedAt: new Date(2016, 5, 10, 12, 24, 34)
       }, {
@@ -14,6 +15,7 @@ module.exports = {
         last_name: 'Denver',
         email: 'bob@island.com',
         phone: '(250) 123-1264',
+        password: 'Bob123',
         createdAt: new Date(2016, 5, 7, 14, 7, 14),
         updatedAt: new Date(2016, 5, 7, 14, 7, 14)
       }, {
@@ -21,6 +23,7 @@ module.exports = {
         last_name: 'Carter',
         email: 'sam@sg1.com',
         phone: '(250) 122-1334',
+        password: 'Samantha123',
         createdAt: new Date(2016, 5, 5, 9, 34, 45),
         updatedAt: new Date(2016, 5, 5, 9, 34, 45)
       }, {
@@ -28,6 +31,7 @@ module.exports = {
         last_name: 'Cooper',
         email: 'alice@nightmare.com',
         phone: '(250) 123-4560',
+        password: 'Alice123',
         createdAt: new Date(2016, 5, 12, 9, 45, 11),
         updatedAt: new Date(2016, 5, 12, 9, 45, 11)
       }, {
@@ -35,6 +39,7 @@ module.exports = {
         last_name: 'McKenzie',
         email: 'doug@hoser.com',
         phone: '(250) 123-4569',
+        password: 'Doug123',
         createdAt: new Date(2016, 5, 11, 14, 34, 14),
         updatedAt: new Date(2016, 5, 11, 14, 34, 14)
       }],
