cff08108528b31dbb38c1e3d9ef7187dccb71da9
Ryan Goodhew <ryangoodhew@gmail.com>
2016-05-17 20:23:56 -0700

    controller login/logout redirects

--- controllers/login.js
+++ controllers/login.js
@@ -2,13 +2,18 @@ var passport = require('passport');
 
 module.exports = function(app) {
   app.get('/login/:usertype?/:id?', function (req, res) {
-    console.log(req.params);
-    res.render('login', {redirect: req.query.redirect});
+    res.render('login');
   });
 
   app.post('/login', 
     passport.authenticate('local', { failureRedirect: '/login' }), function(req, res, next) {
-      return res.redirect('/');  
+      if (req.session.redirectUrl){
+         res.redirect(req.session.redirectUrl);
+         req.session.redirectUrl = null
+         return 
+       }
+      else
+        return res.redirect('/');  
   });
   
 };
--- controllers/logout.js
+++ controllers/logout.js
@@ -2,10 +2,12 @@
 
 module.exports = function(app) {
   app.get('/logout', function(req, res){
-    var name = req.user.first_name;
-    console.log("LOGGING OUT " + req.user.first_name)
-    req.logout();
-    res.redirect('/');
-    req.session.notice = "You have successfully been logged out " + name + "!";
+    if (req.user) {
+      var name = req.user.first_name;
+      console.log("LOGGING OUT " + req.user.first_name)
+      req.logout();
+      req.session.notice = "You have successfully been logged out " + name + "!";
+      }
+      res.redirect('/');
   });
 }
