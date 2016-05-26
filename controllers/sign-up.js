4b2d57c6365413e860d4f977d5ec3898541ed72f
BenByer <benbyer@shaw.ca>
2016-05-25 17:06:56 -0700

    Prevent duplicate email addresses to register

--- controllers/sign-up.js
+++ controllers/sign-up.js
@@ -7,30 +7,40 @@ module.exports = function(app) {
   });
 
   app.post('/sign-up', function(req, res) {
-    console.log(req.body);
-    var user = models.User.create({
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email,
-      phone: req.body.phone,
-      password: req.body.password
-    
-    }).then (function(user) {
-    console.log(user);
-    req.login(user, function(err){
-       if (req.session.redirectUrl) {
-         res.redirect(req.session.redirectUrl);
-         req.session.redirectUrl = null
-         return 
-       }
+    models.User.findOne({ where: {email: req.body.email} }).then(function(emailExists) {
+      console.log("/sign-up", req.body.email, emailExists);
+      if (!emailExists) {
+        var user = models.User.create({
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          email: req.body.email,
+          phone: req.body.phone,
+          password: req.body.password
+        
+        }).then (function(user) {
+        console.log(user);
+        req.login(user, function(err){
+           if (req.session.redirectUrl) {
+             res.redirect(req.session.redirectUrl);
+             req.session.redirectUrl = null
+             return 
+           }
+          else
+            return res.redirect('/');  
+          });  
+              
+        }).catch(function(err) {
+          console.log(err);
+          res.send('BAAAADDDDDDD:   G-' + err.message);
+        });
+      }
       else
-        return res.redirect('/');  
-      });  
-          
-    }).catch(function(err) {
-      console.log(err);
-      res.send('BAAAADDDDDDD:   G-' + err.message);
+      {
+        var data = { emailExists: req.body.email };
+        console.log("emailExists entry failed", data);
+       
+        res.render('sign-up', data);
+      }
     });
-
   });
 }
